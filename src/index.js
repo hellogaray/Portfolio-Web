d65a81bd1a8f8aa6269bd8da781e75be57b07003
Leo <137473678+hellogaray@users.noreply.github.com>
2024-08-12 22:17:26 +0900

    Adding shadow

--- src/index.js
+++ src/index.js
@@ -5,22 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initialize Sortable on the sortable-container
     const sortable = new Sortable(document.querySelector('.sortable-container'), {
         draggable: '.sortable-item',
+        mirror: {
+            constrainDimensions: true,
+          },
         plugins: [Plugins.SortAnimation],
         sortAnimation: {
             duration: 250,
             easingFunction: 'ease-in-out',
         },
-    });
-
-    // Add classes during dragging to maintain the size
-    sortable.on('drag:start', (e) => {
-        e.data.source.style.width = `${e.data.source.clientWidth}px`;
-        e.data.source.style.height = `${e.data.source.clientHeight}px`;
-    });
-
-    // Cleanup after dragging ends
-    sortable.on('drag:stop', (e) => {
-        e.data.source.style.width = '';
-        e.data.source.style.height = '';
     });
 });
