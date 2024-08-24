142305af6b58e4394a8962113dc56bdd985db8f4
Leo <137473678+hellogaray@users.noreply.github.com>
2024-08-24 20:24:11 +0900

    Updated menu

--- src/index.js
+++ src/index.js
@@ -28,3 +28,25 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     });
 });
+
+// Hamburger Button
+const hb_button = document.querySelectorAll("button");
+const sideBar = document.getElementById('sideBar');
+
+hb_button.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentState = button.getAttribute("data-state");
+
+    if (!currentState || currentState === "closed") {
+      button.setAttribute("data-state", "opened");
+      button.setAttribute("aria-expanded", "true");
+      sideBar.classList.add('open');
+      sideBar.classList.remove('close');
+    } else {
+      button.setAttribute("data-state", "closed");
+      button.setAttribute("aria-expanded", "false");
+      sideBar.classList.add('close');
+      sideBar.classList.remove('open');
+    }
+  });
+});
