e1990a52f715792dd176a6f666cde5a9324f18f4
Leo <137473678+hellogaray@users.noreply.github.com>
2024-09-19 22:11:04 +0900

    Working on logo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/modern-normalize.css';
 import './styles/index.css';
 import './styles/components/header.css';
+import './styles/components/hero.css';
 import './styles/utils.css';
 
 import { Sortable, Plugins } from '@shopify/draggable';
